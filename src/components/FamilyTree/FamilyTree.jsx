a778149125c8e77e8f1e78a27e1fa595d7b18161
jonnyjackson26 <jrsjackson26@gmail.com>
2024-05-08 15:57:11 -0600

    improved style and removed errors

--- src/components/FamilyTree/FamilyTree.jsx
+++ src/components/FamilyTree/FamilyTree.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import "./FamilyTree.css"
 import characters from '../../../public/data/characters';
+import { Person } from "./Person.jsx"
 
 
 export function FamilyTree({ character }) {
@@ -9,34 +10,26 @@ export function FamilyTree({ character }) {
     const father = character.fatherId != null ? getCharacterById(character.fatherId) : null;
     const mother = character.motherId != null ? getCharacterById(character.motherId) : null;
     const children = character.childrenIDs != null ? getCharactersByIds(character.childrenIDs) : null;
-    const siblings = (father.childrenIDs != null || mother.childrenIDs != null) ? getCharactersByIds(father.childrenIDs.filter(id => mother.childrenIDs.includes(id) && id !== character.id)) : null; //the common values shared between father.childrenIDs and mother.childrenIDs and not yourself
+    const siblings = ((father && father.childrenIDs != null) && (mother && mother.childrenIDs != null)) ? getCharactersByIds(father.childrenIDs.filter(id => mother.childrenIDs.includes(id) && id !== character.id)) : null; //the common values shared between father.childrenIDs and mother.childrenIDs and not yourself
     return (
         <>
             <div className="treeBox">
                 <div className="parentsRow">
-                    {father && <div className="father">
-                        <Link key={`${father.id}`} to={`/characters/${father.id}`}>{father.name}</Link>
-                    </div>}
-                    {mother && <div className="mother">
-                        <Link key={`${mother.id}`} to={`/characters/${mother.id}`}>{mother.name}</Link>
-                    </div>}
+                    {father && <Person character={father} relation="father" />}
+                    {mother && <Person character={mother} relation="mother" />}
                 </div>
                 <div className="siblingsRow">
-                    <div className="self">{character.name}</div>
+                    <Person character={character} relation="self" />
                     {siblings &&
-                        siblings.map((sibling, index) => (
-                            <div key={index} className="child">
-                                <Link key={`${sibling.id}`} to={`/characters/${sibling.id}`}>{sibling.name}</Link>
-                            </div>
+                        siblings.map((sibling) => (
+                            <Person character={sibling} relation="sibling" />
                         ))
                     }
                 </div>
                 <div className="childrensRow">
                     {children &&
-                        children.map((child, index) => (
-                            <div key={index} className="child">
-                                <Link key={`${child.id}`} to={`/characters/${child.id}`}>{child.name}</Link>
-                            </div>
+                        children.map((child) => (
+                            <Person character={child} relation="child" />
                         ))
                     }
                 </div>
