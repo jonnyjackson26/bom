84d16d4277f231772eae9d20a4f60d1d0adfe3eb
jonnyjackson26 <jrsjackson26@gmail.com>
2024-05-08 14:21:19 -0600

    the most basic familyTree, parents, self, and children

--- src/components/FamilyTree/FamilyTree.jsx
+++ src/components/FamilyTree/FamilyTree.jsx
@@ -1,12 +1,29 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import "./FamilyTree.css"
+import characters from '../../../public/data/characters';
 
 
 export function FamilyTree({ character }) {
+    //basic family tree: shows father and mother, siblings with father and mother, and children
     return (
         <>
-            <h1>{character.name}</h1>
+            <div className="treeBox">
+                <div className="parentsRow">
+                    {character.fatherId != null ? <div className="father">{character.fatherId}</div> : ''}
+                    {character.motherId != null ? <div className="mother">{character.motherId}</div> : ''}
+                </div>
+                <div className="siblingsRow">
+                    <div className="selectedCharacter">{character.name}</div>
+                </div>
+                <div className="childrensRow">
+                    {character.childrenIDs &&
+                        character.childrenIDs.map((childId, index) => (
+                            <div key={index} className="child">{childId}</div>
+                        ))
+                    }
+                </div>
+            </div>
         </>
     );
 }
