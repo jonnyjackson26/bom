efa46cdee7574bde5e2000eaae968f9d3581b5c0
jonnyjackson26 <jrsjackson26@gmail.com>
2024-05-09 11:40:33 -0600

    timeline has vertical line and alternating events but needs more css

--- src/pages/TimelinePage.jsx
+++ src/pages/TimelinePage.jsx
@@ -4,7 +4,7 @@ import NavBar from '../components/NavBar/NavBar.jsx';
 import DocumentTitle from "../components/DocumentTitle.jsx";
 import timelineEvents from '../../public/data/timeline/timelineEvents.js';
 import { TimelineEvent } from "../components/TimelineEvent/TimelineEvent.jsx"
-
+import "../pages/TimelinePage.css"
 
 export function TimelinePage() {
     DocumentTitle("Timeline from Book of Mormon");
@@ -14,9 +14,14 @@ export function TimelinePage() {
 
             <h1>Book of Mormon Timeline</h1>
 
-            {timelineEvents.map((event) => (
-                <TimelineEvent key={event.id} event={event} />
-            ))}
+            <div className="timeline">
+                <div className="line"></div>
+                <div className="events-container">
+                    {timelineEvents.map((event, index) => (
+                        < TimelineEvent key={event.id} event={event} index={index} />
+                    ))}
+                </div>
+            </div>
         </>
 
     );
